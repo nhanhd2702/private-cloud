41544c76095722279dddb4f873b775c861c98129
nhanhd2702 <nhanhd2702@gmail.com>
2023-02-10 10:23:24 +0700

    Update

--- basic-deployment/script_deploy_aio.sh
+++ basic-deployment/script_deploy_aio.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #Define variables
-
-cinder_volumes_disk="/dev/sdb"
-network_if="ens160"
-neutron_ext_if="ens192"
-int_vip_address="192.168.100.10"
+echo "Enter Internal Network Interface Name:"
+read network_if
+echo "Enter External Network Interface Name:"
+read neutron_ex_if
+echo "Enter Internal VIP Address"
+read int_vip_address
 
 #Check privilege 
 die() {
@@ -18,11 +19,17 @@ apt autoremove -y
 #Set date-time
 timedatectl set-timezone Asia/Ho_Chi_Minh
 #Checking cinders-volume
-if vgdisplay | grep -q 'cinder-volumes'; then
-  echo "Ch"
+if vgdisplay |! grep -q 'cinder-volumes'; then
+	echo "Storage not found. Please enter cinder volume disk (example: /dev/sdb)"
+	read cinder_volumes_disk
+	umount $cinder_volumes_disk
+	mkfs.ext4 $cinder_volumes_disk
+	mount $cinder_volumes_disk
+	pvcreate --force $cinder_volumes_disk
+	vgcreate $cinder_volumes_disk
+
 else
-  echo "Storage not found. Please create cinder-volumes to continue... "
-  exit 1;
+  echo "Checking cinder_volumes if OK "
 fi
 
 #Install python libraries & packages
@@ -70,8 +77,8 @@ cp globals.yml globals.bak
 # Git clone config file template
 cd ~
 git clone https://github.com/nhanhd2702/private-cloud-templates.git
-cp private-cloud-templates/all-in-one-basic/all-in-one /etc/kolla/
-cp private-cloud-templates/all-in-one-basic/globals.yml /etc/kolla/
+cp private-cloud-templates/libs/aio/all-in-one /etc/kolla/
+cp private-cloud-templates/libs/aio/globals.yml /etc/kolla/
 #Update globals.yml 
 sed -i 's/$network_if/int_if/' /etc/kolla/globals.yml
 sed -i 's/$neutron_ext_if/ext_if/' /etc/kolla/globals.yml
@@ -80,9 +87,9 @@ sed -i 's/$int_vip_address/int_vip_ip/' /etc/kolla/globals.yml
 #Generate setup passwords
 kolla-genpwd
 ## Deployment
-byobu
-source $HOME/private-cloud/bin/activate
-kolla-ansible -i /etc/kolla/all-in-one bootstrap-servers
-kolla-ansible -i /etc/kolla/all-in-one prechecks
+#byobu
+#source $HOME/private-cloud/bin/activate
+#kolla-ansible -i /etc/kolla/all-in-one bootstrap-servers
+#kolla-ansible -i /etc/kolla/all-in-one prechecks
 
 
