8bf985516d3f31a8d904b35eef7c1121c85686ac
Huỳnh Đắc Nhân <48781310+nhanhd2702@users.noreply.github.com>
2023-02-13 15:55:08 +0700

    Update script_deploy_aio.sh

--- basic-deployment/script_deploy_aio.sh
+++ basic-deployment/script_deploy_aio.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#Define variables
+
 echo "###############################################################"
 echo "#                                                             #"
-echo "#                     NOTICE BOARD                            #"
-echo "#                                                             #"
-echo "#  You are about to start the installation process.           #"
-echo "#  Please make sure that all the required dependencies        #"
-echo "#  are installed and all the necessary files are in place.    #"
-echo "#                                                             #"
+echo "#  You are about to deploy a private-cloud all-in-one system. #"
 echo "#  Press [ENTER] to start the installation process            #"
-echo "#  or press [CTRL+C] to exit                                 #"
+echo "#  or press [CTRL+C] to exit                                  #"
 echo "#                                                             #"
 echo "###############################################################"
 
@@ -19,16 +14,18 @@ read
 # Start the installation process
 echo "Starting the installation process..."
 
+##Define variables
+
 echo "Enter Internal Network Interface Name (Example: ens160)"
 read network_if
 
 echo "Enter External Network Interface Name (Example: ens192)"
-read neutron_ex_if
+read neutron_ext_if
 
 echo "Enter Internal VIP Address (Example: 192.168.10.10)" 
 read int_vip_address
 
-#Check privilege 
+##Check privilege 
 echo "Checking privileges"
 die() {
   echo "ERROR: $1"
@@ -40,8 +37,9 @@ die() {
 ##Create cinder volumes
 #Check cinder-volumes is exist
 echo "Checking cinder-volumes"
-if ! vgdisplay | grep -d "cinder-volumes"; then
-
+if ! vgdisplay | grep -q "cinder-volumes"; then
+	echo "Storage not setup. Please enter name of disk for cinder usage (Example: sdb)"
+	read cinder_volumes_disk
 	if mount | grep $cinder_volumes_disk; then
         umount /dev/$cinder_volumes_disk
         fi
@@ -50,19 +48,20 @@ if ! vgdisplay | grep -d "cinder-volumes"; then
         pvcreate /dev/$cinder_volumes_disk
 
         # Create a volume group
-        vgcreate vg_$cinder_volumes_disk /dev/$cinder_volumes_disk
+        vgcreate cinder-volumes /dev/$cinder_volumes_disk
 
         # Create a logical volume using all the free space on the disk
-        lvcreate -l 100%FREE -n lv_$cinder_volumes_disk vg_$cinder_volumes_disk
+        lvcreate -l 100%FREE -n lv_cinder_volumes cinder-volumes
 
         # Format the logical volume with the ext4 file system
-        mkfs.ext4 /dev/vg_$cinder_volumes_disk/lv_$cinder_volumes_disk
+        mkfs.ext4 /dev/cinder-volumes/lv_cinder_volumes
 
         # Mount the logical volume to a directory
         mkdir /mnt/$cinder_volumes_disk
-        mount /dev/vg_$cinder_volumes_disk/lv_$cinder_volumes_disk /mnt/$cinder_volumes_disk
+        mount /dev/cinder-volumes/lv_cinder_volumes /mnt/$cinder_volumes_disk
 else
         echo "Cinder-volumes is ready for deploy"
+fi
 
 ##Update system packages
 echo "Updating your system packages"
@@ -136,7 +135,7 @@ echo "Create kolla config directory"
 [ ! -d "/etc/kolla" ] && mkdir -p /etc/kolla
 chown $USER:$USER /etc/kolla
 
-#-	Copy file config template
+#Copy file config template
 echo "Copying config teplate files"
 cp -r $HOME/private-cloud/share/kolla-ansible/etc_examples/kolla/* /etc/kolla/
 cp -r $HOME/private-cloud/share/kolla-ansible/ansible/inventory/* /etc/kolla/
@@ -147,21 +146,30 @@ cd /etc/kolla
 cp all-in-one all-in-one.bak
 cp globals.yml globals.bak
 
-# Git clone config file template
+##Git clone config file template
+echo "Clone config template files"
 cd $HOME
 [ -d "private-cloud-templates" ] && rm -rf private-cloud-templates
 git clone https://github.com/nhanhd2702/private-cloud-templates.git
 cp private-cloud-templates/libs/aio/all-in-one /etc/kolla/
 cp private-cloud-templates/libs/aio/globals.yml /etc/kolla/
+
 #Update globals.yml 
-sed -i 's/int_if/$network_if' /etc/kolla/globals.yml
-sed -i 's/ext_if/$neutron_ext_if' /etc/kolla/globals.yml
-sed -i 's/int_vip_ip/$int_vip_address' /etc/kolla/globals.yml
+echo "Update global variables"
+sed -i "s/int_if/$network_if/" /etc/kolla/globals.yml
+sed -i "s/ext_if/$neutron_ext_if/" /etc/kolla/globals.yml
+sed -i "s/int_vip_ip/$int_vip_address/" /etc/kolla/globals.yml
 #
-#Generate setup passwords
+##Generate setup passwords
+echo "Generate setup passwords"
 kolla-genpwd
-## Deployment
-#byobu
-#source $HOME/private-cloud/bin/activate
-#kolla-ansible -i /etc/kolla/all-in-one bootstrap-servers
-#kolla-ansible -i /etc/kolla/all-in-one prechecks
+
+## Finish pre-config for openstack 
+echo "+--------------------------------------------------------+"
+echo "|                                                        |"
+echo "|  Preparation is complete.                              |"
+echo "|  Please run the installation script to continue.       |"
+echo "|                                                        |"
+echo "+--------------------------------------------------------+"
+echo "Press Enter to continue."
+read -p ""
