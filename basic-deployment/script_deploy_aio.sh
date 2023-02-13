634693da4ce458029881c9c6f1f0e38d1966cdbb
Huỳnh Đắc Nhân <48781310+nhanhd2702@users.noreply.github.com>
2023-02-13 11:09:28 +0700

    Update script_deploy_aio.sh

--- basic-deployment/script_deploy_aio.sh
+++ basic-deployment/script_deploy_aio.sh
@@ -1,65 +1,128 @@
 #!/bin/bash
 #Define variables
-echo "Enter Internal Network Interface Name:"
+echo "###############################################################"
+echo "#                                                             #"
+echo "#                     NOTICE BOARD                            #"
+echo "#                                                             #"
+echo "#  You are about to start the installation process.           #"
+echo "#  Please make sure that all the required dependencies        #"
+echo "#  are installed and all the necessary files are in place.    #"
+echo "#                                                             #"
+echo "#  Press [ENTER] to start the installation process            #"
+echo "#  or press [CTRL+C] to exit                                 #"
+echo "#                                                             #"
+echo "###############################################################"
+
+# Wait for the user to press [ENTER]
+read
+
+# Start the installation process
+echo "Starting the installation process..."
+
+echo "Enter Internal Network Interface Name (Example: ens160)"
 read network_if
-echo "Enter External Network Interface Name:"
+
+echo "Enter External Network Interface Name (Example: ens192)"
 read neutron_ex_if
-echo "Enter Internal VIP Address"
+
+echo "Enter Internal VIP Address (Example: 192.168.10.10)" 
 read int_vip_address
 
 #Check privilege 
+echo "Checking privileges"
 die() {
   echo "ERROR: $1"
   exit 1
 }
-[ $(whoami) = "root" ] || die "This script must be run as root"
-#Update system packages
-apt update && sudo apt upgrade -y
-apt autoremove -y
-#Set date-time
-timedatectl set-timezone Asia/Ho_Chi_Minh
-#Checking cinders-volume
-if vgdisplay |! grep -q 'cinder-volumes'; then
-	echo "Storage not found. Please enter cinder volume disk (example: /dev/sdb)"
-	read cinder_volumes_disk
-	umount $cinder_volumes_disk
-	mkfs.ext4 $cinder_volumes_disk
-	mount $cinder_volumes_disk
-	pvcreate --force $cinder_volumes_disk
-	vgcreate $cinder_volumes_disk
 
+[ $(whoami) = "root" ] || die "This script must be run as root"
+
+##Create cinder volumes
+#Check cinder-volumes is exist
+echo "Checking cinder-volumes"
+if ! vgdisplay | grep -d "cinder-volumes"; then
+
+	if mount | grep $cinder_volumes_disk; then
+        umount /dev/$cinder_volumes_disk
+        fi
+
+        # Create a physical volume for LVM
+        pvcreate /dev/$cinder_volumes_disk
+
+        # Create a volume group
+        vgcreate vg_$cinder_volumes_disk /dev/$cinder_volumes_disk
+
+        # Create a logical volume using all the free space on the disk
+        lvcreate -l 100%FREE -n lv_$cinder_volumes_disk vg_$cinder_volumes_disk
+
+        # Format the logical volume with the ext4 file system
+        mkfs.ext4 /dev/vg_$cinder_volumes_disk/lv_$cinder_volumes_disk
+
+        # Mount the logical volume to a directory
+        mkdir /mnt/$cinder_volumes_disk
+        mount /dev/vg_$cinder_volumes_disk/lv_$cinder_volumes_disk /mnt/$cinder_volumes_disk
 else
-  echo "Checking cinder_volumes if OK "
-fi
+        echo "Cinder-volumes is ready for deploy"
+
+##Update system packages
+echo "Updating your system packages"
+
+apt update && sudo apt upgrade -y
+
+apt autoremove -y
+
+#Set date-time
+echo "Setting date & time"
+
+timedatectl set-timezone Asia/Ho_Chi_Minh
 
 #Install python libraries & packages
+
+echo "Install python libraries & packages"
 apt install python3-pip python3-dev python3-docker libffi-dev gcc libssl-dev git -y
+
 #Install virtualenv
+echo "Setting up virtual environment"
 apt install python3-venv python3-virtualenv -y
+
 #Create virtual environment
+
 if [ ! -d "$HOME/private-cloud" ]; then
 python3 -m venv $HOME/private-cloud
+
 else
 	rm -rf "$HOME/private-cloud"
 	python3 -m venv $HOME/private-cloud
 fi
+
 # Activate virtual environment
+echo "Active venv & setting up kolla packages"
 source $HOME/private-cloud/bin/activate
+
 #Upgrade pip tools
 pip install -U pip
+
 #Install Ansible
 pip install -U 'ansible>=4,<6'
+
 #Install kolla-ansible and its dependencies using pip
 pip install "kolla-ansible==15.0.0"
+
 #pip install git+https://opendev.org/openstack/kolla-ansible@stable/zed
+
 #Install Ansible Galaxy requirements
 kolla-ansible install-deps
+
 ##Prepare configs file for deployment
+
 #Create & tuning ansible
+echo "Tunning ansible configs"
+
 if [[ ! -e /etc/ansible/ansible.cfg ]]; then
     mkdir -p /etc/ansible
     touch /etc/ansible/ansible.cfg
 fi
+
 #Tunning ansible
 cat << EOF > /etc/ansible/ansible.cfg
 [defaults]
@@ -67,25 +130,33 @@ host_key_checking=False
 pipelining=True
 forks=100
 EOF
+
 #Create the /etc/kolla directory
+echo "Create kolla config directory"
 [ ! -d "/etc/kolla" ] && mkdir -p /etc/kolla
 chown $USER:$USER /etc/kolla
+
 #-	Copy file config template
+echo "Copying config teplate files"
 cp -r $HOME/private-cloud/share/kolla-ansible/etc_examples/kolla/* /etc/kolla/
 cp -r $HOME/private-cloud/share/kolla-ansible/ansible/inventory/* /etc/kolla/
+
 #Backup original config files
+echo "Backing up config files"
 cd /etc/kolla
 cp all-in-one all-in-one.bak
 cp globals.yml globals.bak
+
 # Git clone config file template
-cd ~
+cd $HOME
+[ -d "private-cloud-templates" ] && rm -rf private-cloud-templates
 git clone https://github.com/nhanhd2702/private-cloud-templates.git
 cp private-cloud-templates/libs/aio/all-in-one /etc/kolla/
 cp private-cloud-templates/libs/aio/globals.yml /etc/kolla/
 #Update globals.yml 
-sed -i 's/$network_if/int_if/' /etc/kolla/globals.yml
-sed -i 's/$neutron_ext_if/ext_if/' /etc/kolla/globals.yml
-sed -i 's/$int_vip_address/int_vip_ip/' /etc/kolla/globals.yml
+sed -i 's/int_if/$network_if' /etc/kolla/globals.yml
+sed -i 's/ext_if/$neutron_ext_if' /etc/kolla/globals.yml
+sed -i 's/int_vip_ip/$int_vip_address' /etc/kolla/globals.yml
 #
 #Generate setup passwords
 kolla-genpwd
@@ -94,5 +165,3 @@ kolla-genpwd
 #source $HOME/private-cloud/bin/activate
 #kolla-ansible -i /etc/kolla/all-in-one bootstrap-servers
 #kolla-ansible -i /etc/kolla/all-in-one prechecks
-
-
