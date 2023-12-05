34c7c667175db62a0709e40a389be22ee0b18978
Huỳnh Đắc Nhân <48781310+nhanhd2702@users.noreply.github.com>
2023-12-05 12:24:10 +0700

    Update 03.pre-setup_multinode.sh

--- basic-deployment/03.pre-setup_multinode.sh
+++ basic-deployment/03.pre-setup_multinode.sh
@@ -10,12 +10,14 @@ die() {
 [ "$(whoami)" = "root" ] || die "This script must be run as root"
 
 # Servers definition
+declare -a sv_host_array
 declare -a neutron_int_if_array
 declare -a neutron_ext_if_array
 config_line=""
 echo "Enter the number of servers: "
 read -r num_servers
 
+# Server information.
 for ((i=1; i<=num_servers; i++))
 do
     read -r -p "Enter the name of server $i (example: serv-0$i): " sv_host
@@ -27,6 +29,7 @@ do
     neutron_int_if_array[i]=$neutron_int_if
     neutron_ext_if_array[i]=$neutron_ext_if
     config_line="$config_line\n$sv_host ansible_ssh_host=$sv_ip ansible_connection=ssh ansible_user=honeynet ansible_sudo_pass=honeynet.vn network_interface=$neutron_int_if neutron_external_interface=$neutron_ext_if"
+    sv_host_array[i]=$sv_host
 done
 read -r -p "Enter Internal VIP Address (example: 192.168.10.100): " int_vip_address
 
@@ -40,8 +43,14 @@ ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 # Copy the SSH public key to the user's authorized_keys file
 for ((i=1; i<=num_servers; i++))
 do
-    ssh-copy-id -i ~/.ssh/id_rsa.pub "$login_name@${sv_ip[i]}"
+    # Copy the SSH public key to the user's authorized_keys file
+    if ssh-copy-id -i ~/.ssh/id_rsa.pub "$login_name@${sv_host_array[$i]}"; then
+        echo "SSH key copied to $login_name@${sv_host_array[$i]}"
+    else
+        echo "Failed to copy SSH key to $login_name@${sv_host_array[$i]}"
+    fi
 done
+
 
 ## Update system packages
 echo "Updating your system packages"
@@ -64,16 +73,16 @@ echo "Setting up virtual environment"
 apt install python3-venv python3-virtualenv -y
 
 # Create virtual environment
-if [ ! -d "$HOME/private-cloud" ]; then
-    python3 -m venv "$HOME/private-cloud"
+if [ ! -d "/usr/local/private-cloud" ]; then
+    python3 -m venv "/usr/local/private-cloud"
 else
-    rm -rf "$HOME/private-cloud"
-    python3 -m venv "$HOME/private-cloud"
+    rm -rf "/usr/local/private-cloud"
+    python3 -m venv "/usr/local/private-cloud"
 fi
 
 # Activate virtual environment
 echo "Activate venv & setting up kolla packages"
-source "$HOME/private-cloud/bin/activate" || exit
+source "/usr/local/private-cloud/bin/activate" || exit
 
 # Upgrade pip tools
 pip install -U pip
@@ -82,7 +91,7 @@ pip install -U pip
 pip install -U 'ansible>=4,<6'
 
 # Install kolla-ansible and its dependencies using pip
-pip install "kolla-ansible==15.2.0"
+pip install "kolla-ansible==15.3.0"
 
 # pip install git+https://opendev.org/openstack/kolla-ansible@stable/zed
 
@@ -114,8 +123,8 @@ chown "$USER:$USER" /etc/kolla
 
 # Copy file config template
 echo "Copying config template files"
-cp -r $HOME/private-cloud/share/kolla-ansible/etc_examples/kolla/* /etc/kolla/
-cp -r $HOME/private-cloud/share/kolla-ansible/ansible/inventory/* /etc/kolla/
+cp -r /usr/local/private-cloud/share/kolla-ansible/etc_examples/kolla/* /etc/kolla/
+cp -r /usr/local/private-cloud/share/kolla-ansible/ansible/inventory/* /etc/kolla/
 
 # Backup original config files
 echo "Backing up config files"
@@ -125,12 +134,10 @@ cp globals.yml globals.bak
 
 ## Git clone config file template
 echo "Clone config template files"
-cd "$HOME" || exit
-[ -d "private-cloud-templates" ] && rm -rf private-cloud-templates
-git clone https://github.com/nhanhd2702/private-cloud-templates.git
-cp private-cloud-templates/libs/multinode/multinode /etc/kolla/
-cp private-cloud-templates/libs/multinode/globals.yml /etc/kolla/
-cp -R private-cloud-templates/config /etc/kolla/
+[ -d "/tmp/private-cloud-templates" ] && rm -rf "/tmp/private-cloud-templates" && git clone https://github.com/nhanhd2702/private-cloud-templates.git
+cp /tmp/private-cloud-templates/basic-deployment/libs/multinode/multinode /etc/kolla/
+cp /tmp/private-cloud-templates//basic-deployment/libs/multinode/globals.yml /etc/kolla/
+cp -R /tmp/private-cloud-templates/config /etc/kolla/
 
 # Edit inventory file
 echo "Append the servers to the inventory file"
