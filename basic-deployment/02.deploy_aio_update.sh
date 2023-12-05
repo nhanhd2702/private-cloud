a7fad0f778beb4186893adabc79358f573e6384c
Huỳnh Đắc Nhân <48781310+nhanhd2702@users.noreply.github.com>
2023-12-05 14:01:41 +0700

    Update 02.deploy_aio_update.sh

--- basic-deployment/02.deploy_aio_update.sh
+++ basic-deployment/02.deploy_aio_update.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 if byobu list-sessions 2>&1 | grep -q "no server running"; then
-    echo "No active Byobu session. Exiting."
-    exit 1
-else
-    echo "Byobu is active. Start deploying the private-cloud.."
-    source "$HOME/private-cloud/bin/activate"
-
-    # Run kolla-ansible bootstrap-servers command
-    if ! kolla-ansible -i /etc/kolla/all-in-one bootstrap-servers; then
-        echo "An error occurred during bootstrap-servers."
-        exit 1
-    fi
-
-    # If all commands succeed, you can exit with a success status
-    echo "kolla-ansible deploy successfully."
-    echo "Continuing to post-deploy."
-
-    if ! kolla-ansible -i /etc/kolla/all-in-one post-deploy; then
-        echo "An error occurred during post-deploy."
-        exit 1
-    fi
-
-    pip install python-openstackclient python-glanceclient python-neutronclient
-    source /etc/kolla/admin-openrc.sh
-    if ! openstack token issue; then
-        echo "An error occurred when issuing the OpenStack token."
-        exit 1
-    fi
-
-    admin_password=$(grep keystone_admin /etc/kolla/passwords.yml)
-    echo "kolla-ansible post-deploy completed successfully. The admin password is $admin_password"
-    exit 0
+    echo "No active Byobu session. Exiting." && exit 1
 fi
+
+echo "Byobu is active. Start deploying the private-cloud.."
+source $HOME/private-cloud/bin/activate
+
+# Functions for command execution and error handling
+execute_command() {
+    $1
+    if [ $? -ne 0 ]; then
+        echo "An error occurred during $2."
+        exit 1
+    fi
+}
+
+# Run Kolla-ansible commands with error handling
+execute_command "kolla-ansible -i /etc/kolla/all-in-one bootstrap-servers" "bootstrap-servers"
+execute_command "kolla-ansible -i /etc/kolla/all-in-one prechecks" "prechecks"
+execute_command "kolla-ansible -i /etc/kolla/all-in-one deploy" "deploy"
+execute_command "kolla-ansible post-deploy" "post-deploy"
+
+# Install OpenStack clients, source admin credentials, and check token issuance
+pip install python-openstackclient python-glanceclient python-neutronclient
+source /etc/kolla/admin-openrc.sh
+openstack token issue
+
+# Print admin password
+admin_password=$(grep keystone_admin /etc/kolla/passwords.yml)
+echo "kolla-ansible post-deploy completed successfully. The admin password is $admin_password"
+exit 0
